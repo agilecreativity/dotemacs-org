8d074dc004dcd2fdcf7d78387ead04b739807527
Burin Choomnuan <agilecreativity@gmail.com>
2016-01-31 15:45:19 +1100

    Initial Import

---
+++ custom.el
@@ -0,0 +1,42 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(custom-safe-themes
+   (quote
+    ("f0a99f53cbf7b004ba0c1760aa14fd70f2eabafe4e62a2b3cf5cabae8203113b" default)))
+ '(helm-locate-command "mdfind -interpret -name %s %s")
+ '(helm-mode t)
+ '(package-selected-packages
+   (quote
+    (scratch zoom-frm emmet-mode yasnippet visual-regexp-steroids visual-regexp smooth-scrolling skewer-mode smex smartscan smartparens geiser racket-mode ag olivetti multiple-cursors markdown-mode macrostep gist flycheck expand-region ein edit-server magit helm-ag helm-projectile helm dash-at-point clojure-mode c-eldoc android-mode ace-window ace-jump-mode ido-vertical-mode transpose-frame ox-pandoc exec-path-from-shell solarized-theme use-package)))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
